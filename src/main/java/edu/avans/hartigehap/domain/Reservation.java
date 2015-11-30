4231da425412829ef4662f3480f07e21d17e24b2
Luc Koevoets <lacrkoev@avans.nl>
2015-11-30 20:39:46 +0100

    Aanvulling gedaan

--- src/main/java/edu/avans/hartigehap/domain/Reservation.java
+++ src/main/java/edu/avans/hartigehap/domain/Reservation.java
@@ -3,6 +3,10 @@ package edu.avans.hartigehap.domain;
 import javax.persistence.Entity;
 
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.NotEmpty;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -19,9 +23,15 @@ import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 public class Reservation extends DomainObject {
 	private static final long serialVersionUID = 1L;
 
+	@NotEmpty (message = "{validation.reservation.name.NotEmpty.message}")
 	private String name;
+	
+	@NotEmpty (message = "{validation.reservation.description.NotEmpty.message}")
 	private String description;
-	private int Size;
+	
+	@NotNull (message = "{validation.reservation.groupSize.NotNull.message}")
+	@Size (min=2, max= 99, message = "{validation.reservation.groupSize.Size.message}")
+	private int groupSize;
 	
 	
 	
