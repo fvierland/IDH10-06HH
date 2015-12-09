f3bd31d3ca52561a01c6ab9039511b511db0d55e
Ryanevan17 <ryanevank@msn.com>
2015-12-09 20:18:12 +0100

    John: InvalidReserv..

---
+++ src/main/java/edu/avans/hartigehap/domain/InvalidReservationStatusActionException.java
@@ -0,0 +1,7 @@
+package edu.avans.hartigehap.domain;
+
+public class InvalidReservationStatusActionException extends Exception{
+	   public InvalidReservationStatusActionException(String message) {
+	        super(message);
+	    }
+	}
