62f7b64b829f133e2ed7badb31b26cf9fcad375e
Luc Koevoets <lacrkoev@avans.nl>
2016-01-05 20:39:13 +0100

    IPeriod aangemaakt en de diverse dagdelen

--- src/main/java/edu/avans/hartigehap/domain/IPeriod.java
+++ src/main/java/edu/avans/hartigehap/domain/IPeriod.java
@@ -1,33 +1,40 @@
 package edu.avans.hartigehap.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
+
 
 import org.joda.time.DateTime;
+import org.joda.time.LocalDate;
 
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 
-import lombok.Setter;
 import lombok.Getter;
+import lombok.Setter;
 
 @Entity
-@Table (name ="PERIODS")
+@Table(name = "PERIOD")
 @Getter
 @Setter
+@Inheritance(strategy=InheritanceType.SINGLE_TABLE)
 @JsonIdentityInfo(generator = ObjectIdGenerators.IntSequenceGenerator.class, property = "@id")
-public abstract class Period extends DomainObject{
+public abstract class IPeriod extends DomainObject{
 	private static final long serialVersionUID = 1L;
 
-		
-	@ManyToOne
-	protected Reservation reservation;
+	@Column(name = "DTYPE", insertable = false, updatable = false)
+	private String type;
+
+	protected LocalDate date;
 	
+	@ManyToOne()
+	protected Reservation reservation;
 	
 	public abstract void setDate(DateTime date);
 
-	
+	@Override
+	public String toString() {
+		return this.type;
+	}
 	
 
 }
