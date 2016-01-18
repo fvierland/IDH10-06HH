00f57e9a62c364dc9268c7b8a6844375b55cd3f4
Luc Koevoets <lacrkoev@avans.nl>
2016-01-18 21:28:37 +0100

    Reservering pagina werkend na handmatig vullen DB

--- src/main/java/edu/avans/hartigehap/web/controller/ReservationController.java
+++ src/main/java/edu/avans/hartigehap/web/controller/ReservationController.java
@@ -7,6 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
 import org.springframework.web.servlet.mvc.support.*;
 import org.springframework.validation.*;
 
@@ -29,18 +30,18 @@ public class ReservationController {
     private MessageSource messageSource;
 	
 	@Autowired
+    private FinalStateService finalStateService;
+	
+	@Autowired
     private ReservationService reservationService;
 
     @Autowired
     private RestaurantService restaurantService;
     
-    @Autowired
-    private CustomerService customerService;
+     
+ 
     
-    @Autowired
-    private FacilityService facilityService;
-    
-    //List reservations
+    //List reservations   - WERKT ! 
     @RequestMapping(value = "/restaurants/{restaurantName}/reservations", method = RequestMethod.GET)
     public String listReservations (@PathVariable("restaurantName")String restaurantName, Model uiModel) {
     	warmupRestaurant(restaurantName, uiModel);
@@ -51,9 +52,33 @@ public class ReservationController {
         uiModel.addAttribute("reservations", reservations);
         return "hartigehap/listreservations";
     }
+    // 	show reservation
+    @RequestMapping(value = "/restaurants/{restaurantName}/reservations/{id}", method = RequestMethod.GET)
+    public String showReservation(@PathVariable("restaurantName") String restaurantName, @PathVariable("id") Long id,
+            Model uiModel) {
 
+        warmupRestaurant(restaurantName, uiModel);
+        
+        Reservation reservation = reservationService.findById(id);
+        uiModel.addAttribute("reservation", reservation);
+        return "hartigehap/showcustomer";
+    }
+    
+    
+    // Update reservation
+    @RequestMapping(value = "/restaurants/{restaurantName}/reservations/{id}", params = "form", method = RequestMethod.GET)
+    public String updateReservationForm(@PathVariable("restaurantName") String restaurantName, @PathVariable("id") Long id,
+            Model uiModel) {
+
+        warmupRestaurant(restaurantName, uiModel);
        
-    // Redirect naar Editreservations via creeer reservering
+        Reservation reservation = reservationService.findById(id);
+        uiModel.addAttribute("reservation", reservation);
+        return "hartigehap/editcustomer";
+    }
+    
+       
+    // Create reservation
     @RequestMapping(value = "/restaurants/{restaurantName}/reservations", params = "form", method = RequestMethod.GET)
     public String createReservationForm(@PathVariable("restaurantName") String restaurantName, Model uiModel) {
 
@@ -98,6 +123,7 @@ public class ReservationController {
                 + UrlUtil.encodeUrlPathSegment(reservation.getId().toString(), httpServletRequest);
     }
     
+    
     //Reservering bijwerken
     @RequestMapping(value = "/restaurants/{restaurantName}/reservations/{id}", params = "form", method = RequestMethod.PUT)
     public String updateReservation(
@@ -111,20 +137,22 @@ public class ReservationController {
                 httpServletRequest, redirectAttributes, locale);
         }
     
-    //Reservering aanmaken
-    @RequestMapping(value = "/restaurants/{restaurantName}/newreservations", method = RequestMethod.POST)
-    public String createReservation(@PathVariable("restaurantName") String restaurantName, Model uiModel)          {
-    	List<Customer> customers = this.customerService.findAll();
-        List<IFacility> iFacility = this.facilityService.findByType("Room");
-
-        uiModel.addAttribute("reservation", new Reservation());
-        uiModel.addAttribute("customers", customers);
-        uiModel.addAttribute("ifacility", iFacility);
-        return "hartigehap/newreservations";
-    	
-        
-    }
     
+    
+   // nieuwe reservering
+    @RequestMapping(value = "/restaurants/${restaurant.id}/newreservations", params = "form", method = RequestMethod.GET)
+    public String createReservation (@PathVariable("restaurantName") String restaurantName, @Valid Reservation reservation,
+            BindingResult bindingResult, Model uiModel, HttpServletRequest httpServletRequest,
+            RedirectAttributes redirectAttributes, Locale locale)            {
+    
+    	Restaurant restaurant = warmupRestaurant(restaurantName, uiModel);
+        reservation.setRestaurants(Arrays.asList(new Restaurant[] { restaurant }));
+        // to get the auto generated id
+        reservation = reservationService.save(reservation);
+     
+        return handleCreateOrUpdateReservation(true, restaurantName, reservation, bindingResult, uiModel,
+                httpServletRequest, redirectAttributes, locale);
+}
     
     
     // Reservering verwijderen
@@ -134,6 +162,29 @@ public class ReservationController {
         
         reservationService.delete(id);
         return "redirect:/restaurants/" + restaurantName + "/reservations/";
+    }
+
+    // status van reservering wijzigen
+    @RequestMapping(value = "/reservations/finalize/reservations/{id}", method = RequestMethod.GET)
+    public String makeFinal(Model uiModel, @PathVariable("restaurantName") String restaurantName, @PathVariable("id") Long id) {
+        Reservation reservation = this.reservationService.findById(id);
+
+        if(reservation == null) {
+            uiModel.addAttribute("message", new Message("Error", "Reservation not found"));
+            return this.listReservations(restaurantName, uiModel);
+        }
+
+        try {
+            IReservationState status = reservation.getState();
+            status.setFinalStateService(this.finalStateService);
+            status.setReservationService(this.reservationService);
+            status.makeFinal(reservation);
+            uiModel.addAttribute("message", new Message("success", "State Changed"));
+            return this.listReservations(restaurantName, uiModel);
+        } catch (InvalidStateException exception) {
+            uiModel.addAttribute("message", new Message("error", "State not changed!: " + exception.getMessage()));
+            return this.listReservations(restaurantName, uiModel);
+        }
     }
     
     
